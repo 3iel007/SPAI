1c2c2025f503bdcc5daf79735efaa68c4bfa878b
3iel007 <101736362+3iel007@users.noreply.github.com>
2023-11-24 18:17:11 -0300

    Delete SPAI directory

--- SPAI/dados_spai.sql
+++
@@ -1,5 +0,0 @@
-describe clientes;
-SELECT * FROM clientes;
-
-
-
--- SPAI/testa_conexaospai.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include("conexaospai.php");
-if ($conexao){
-	echo "Conexão criada";
-}else{
-	echo "Erro de conexão";
-}
-?>
